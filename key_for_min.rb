6f816bcc1d0473e202e5ed00a151d57810ea9db3
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 17:44:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  # if conditional statement
+  lowest_value = []
+  name_hash.collect do |name, number|
+    if number == 1
+      lowest_value << name
+    else lowest_value == []
+      nil
+    end
+  end
 end
