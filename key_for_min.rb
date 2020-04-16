84cd1abd0b6ddd4363223c22429f5ca5e09cafc0
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 17:46:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,11 @@
 
 def key_for_min_value(name_hash)
   # if conditional statement
-  lowest_value = []
+  lowest_number = 0
+  empty_hash = nil
   name_hash.collect do |name, number|
-    if number == 1
-      lowest_value << name
-    else lowest_value == []
-      nil
+    if number
+      
     end
   end
 end
